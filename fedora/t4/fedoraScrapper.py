5b9c8654c2205ce5441c8a6a733ed0b28ab49455
Arnav Garg <gargarnav98@gmail.com>
2019-12-21 23:15:25 +0530

    added error message

--- fedora/t4/fedoraScrapper.py
+++ fedora/t4/fedoraScrapper.py
@@ -1,8 +1,10 @@
 from fedora.client.fas2 import AccountSystem
 
 input = raw_input('Please type the username:')
-fas = AccountSystem(username=input)
+fas = AccountSystem(username="gkanav")
 people = fas.person_by_username(str(input))
-print(people)
-print("Email ID of user: " + people["bugzilla_email"])
+if len(people) == 0:
+    print("Sorry, user does not exist.")
+else:
+    print("Email ID of user: " + people["bugzilla_email"])
 
